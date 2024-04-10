042d1df1ab3fcc9b6dc63e1bdac02216ba48231a
Ankit-ICY <ankitbisht981@gmail.com>
2024-04-10 23:14:25 +0530

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# TWL-Employment_Portal
-=======
-# TWL-Project
->>>>>>> 64d7853a2be3f0089589b6ba6692cf4d8a4d9ff4
+git remote add origin https://github.com/Ankit-ICY/TWL-Project.git
--- myjob/settings.py
+++ myjob/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'myjob.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RNDoPMsxvWyiUDwKKnGsbexnCKnBeiKr', 
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '52183',
     }
 }
+
 
 
 # Password validation
