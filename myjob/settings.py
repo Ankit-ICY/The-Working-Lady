2482ed78c57b4296a1e50237fea5ff39e4736a88
Ankit Bisht <110342334+Ankit-ICY@users.noreply.github.com>
2024-04-18 21:10:14 +0530

    Update settings.py

--- myjob/settings.py
+++ myjob/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xy%*(bmv_wq3t@rp&h&rrpe1)q&thz_yorkz^dipr)1d9rzg0l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
