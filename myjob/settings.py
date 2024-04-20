5b5f8ef5b71a2d20ff933112f065acfb66d6dcee
Ankit Bisht <110342334+Ankit-ICY@users.noreply.github.com>
2024-04-20 19:53:01 +0530

    Update settings.py

--- myjob/settings.py
+++ myjob/settings.py
@@ -119,21 +119,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , "static")
+    os.path.join(BASE_DIR , 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'mediafiles')
 
 
 RAZOR_KEY_ID = 'rzp_test_bkUvdizg5rUdQj'
