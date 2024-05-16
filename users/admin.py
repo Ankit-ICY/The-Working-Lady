83e1090395e287624f15f5da93c5f4a2da9dc4f2
Ankit Bisht <110342334+Ankit-ICY@users.noreply.github.com>
2024-05-17 01:44:38 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact, Post, Blog
+from .models import Contact
 from import_export.admin import ImportExportModelAdmin # type: ignore
 
 
@@ -8,8 +8,4 @@ class ContactAdmin(ImportExportModelAdmin, admin.ModelAdmin):
 
 
 admin.site.register(Contact, ContactAdmin)
-admin.site.register(Post)
-admin.site.register(Blog)
-
-
 # Register your models here.
