c9ac2cb82c736470f679e6418c43fa7cafa409e9
Ankit Bisht <110342334+Ankit-ICY@users.noreply.github.com>
2024-05-17 01:23:28 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 
@@ -11,13 +12,28 @@ class Contact(models.Model):
 
     def __str__(self) -> str:
         return self.name
-
+    
 
 class Post(models.Model):
     post_title = models.CharField(max_length=30)
-    post_image = models.ImageField(upload_to='user_photo', null= True, blank = True)
+    post_image = models.ImageField(upload_to='resumes', null= True, blank = True)
     post_para = models.TextField()
 
     class Meta:
         ordering = ['-id'] 
 
+
+
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    content = RichTextField()
+    author = models.CharField(max_length=30, default='',null= True, blank = True)
+    image = models.ImageField(upload_to='resumes', null= True, blank = True)
+    description = models.TextField(default='',null= True, blank = True)
+    date = models.DateField(auto_now_add=True, null=True)
+
+
+    def __str__(self) -> str:
+        return self.title
