7fdaa834710c44ee2a590f27ee74ed8f386e462a
Ankit Bisht <110342334+Ankit-ICY@users.noreply.github.com>
2024-08-11 12:37:45 +0530

    Update settings.py

--- myjob/settings.py
+++ myjob/settings.py
@@ -197,3 +197,13 @@ TINYMCE_DEFAULT_CONFIG = {
     'code_dialog_width': 800,
 }
 
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'theworkinglady.in@gmail.com'  # Your Gmail address
+EMAIL_HOST_PASSWORD = 'boaqnoelrgujsiyy'  # Your Gmail app password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
